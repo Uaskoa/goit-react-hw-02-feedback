c5f9ee618eb161c474b12a3ec344f6c0f6c80dfd
Anna Shevchenko <uaskoa@gmail.com>
2021-04-24 01:08:13 +0300

    styles

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,19 +1,19 @@
-const Controls = ({
-  onGoodFeedback,
-  onNeutralFeedback,
-  onBadFeedback,
-}) => (
-  <div>
-    <button type="button" onClick={onGoodFeedback}>
-      Good
-    </button>
-    <button type="button" onClick={onNeutralFeedback}>
-      Neutral
-    </button>
-    <button type="button" onClick={onBadFeedback}>
-      Bad
-    </button>
-  </div>
-);
+// const Controls = ({
+//   onGoodFeedback,
+//   onNeutralFeedback,
+//   onBadFeedback,
+// }) => (
+//   <div>
+//     <button type="button" onClick={onGoodFeedback}>
+//       Good
+//     </button>
+//     <button type="button" onClick={onNeutralFeedback}>
+//       Neutral
+//     </button>
+//     <button type="button" onClick={onBadFeedback}>
+//       Bad
+//     </button>
+//   </div>
+// );
 
-export default Controls;
+// export default Controls;
--- src/components/FeedbackCounter.jsx
+++ src/components/FeedbackCounter.jsx
@@ -1,93 +1,93 @@
-import { Component } from "react";
-import Controls from "./Controls"
+// import { Component } from "react";
+// import Controls from "./Controls"
 
-class FeedbackCounter extends Component {
-  //   constructor() {
-  //     super();
+// class FeedbackCounter extends Component {
+//   //   constructor() {
+//   //     super();
 
-  //     this.state = {
-  //       good: 0,
-  //       neutral: 0,
-  //       bad: 0,
-  //     };
-  //   }
+//   //     this.state = {
+//   //       good: 0,
+//   //       neutral: 0,
+//   //       bad: 0,
+//   //     };
+//   //   }
 
-  static defaultProps = {
-    initialValue: {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    },
-  };
+//   static defaultProps = {
+//     initialValue: {
+//       good: 0,
+//       neutral: 0,
+//       bad: 0,
+//     },
+//   };
 
-  state = {
-    good: this.props.initialValue.good,
-    neutral: this.props.initialValue.neutral,
-    bad: this.props.initialValue.bad,
-  };
+//   state = {
+//     good: this.props.initialValue.good,
+//     neutral: this.props.initialValue.neutral,
+//     bad: this.props.initialValue.bad,
+//   };
 
-  //   static propTypes = {};
+//   //   static propTypes = {};
 
-  handleGoodFeedback = (event) => {
+//   handleGoodFeedback = (event) => {
     
 
-    this.setState((prevState) => ({ good: prevState.good + 1 }));
+//     this.setState((prevState) => ({ good: prevState.good + 1 }));
 
-    // const target = event.target;
-  };
+//     // const target = event.target;
+//   };
 
-  handleNeutralFeedback = (event) => {
+//   handleNeutralFeedback = (event) => {
     
 
-    this.setState((prevState) => ({ neutral: prevState.neutral + 1 }));
-  };
+//     this.setState((prevState) => ({ neutral: prevState.neutral + 1 }));
+//   };
 
-  handleBadFeedback = (event) => {
+//   handleBadFeedback = (event) => {
     
 
-    this.setState((prevState) => ({ bad: prevState.bad + 1 }));
-  };
+//     this.setState((prevState) => ({ bad: prevState.bad + 1 }));
+//   };
 
     
-    countTotalFeedback = () => {
-        return this.state.good + this.state.neutral + this.state.bad;
-    }
+//     countTotalFeedback = () => {
+//         return this.state.good + this.state.neutral + this.state.bad;
+//     }
 
-    countPositiveFeedbackPercentage = () => {
-        return (this.state.good * 100) / this.countTotalFeedback();
-    }
+//     countPositiveFeedbackPercentage = () => {
+//         return (this.state.good * 100) / this.countTotalFeedback();
+//     }
 
-  render() {
-    return (
-      <div>
-        <h2>Please leave feedback</h2>
+//   render() {
+//     return (
+//       <div>
+//         <h2>Please leave feedback</h2>
 
-        <Controls
-          onGoodFeedback={this.handleGoodFeedback}
-          onNeutralFeedback={this.handleNeutralFeedback}
-          onBadFeedback={this.handleBadFeedback}
-        />
+//         <Controls
+//           onGoodFeedback={this.handleGoodFeedback}
+//           onNeutralFeedback={this.handleNeutralFeedback}
+//           onBadFeedback={this.handleBadFeedback}
+//         />
 
-        {/* <button type="button" onClick={this.handleGoodFeedback}>
-          Good
-        </button>
-        <button type="button" onClick={this.handleNeutralFeedback}>
-          Neutral
-        </button>
-        <button type="button" onClick={this.handleBadFeedback}>
-          Bad
-        </button> */}
+//         {/* <button type="button" onClick={this.handleGoodFeedback}>
+//           Good
+//         </button>
+//         <button type="button" onClick={this.handleNeutralFeedback}>
+//           Neutral
+//         </button>
+//         <button type="button" onClick={this.handleBadFeedback}>
+//           Bad
+//         </button> */}
 
-        <h3>Statistics</h3>
-        <p>Good: {this.state.good}</p>
+//         <h3>Statistics</h3>
+//         <p>Good: {this.state.good}</p>
 
-        <p>Neutral: {this.state.neutral}</p>
-        <p>Bad: {this.state.bad}</p>
-        <p>Total: {this.countTotalFeedback()}</p>
-        <p>Positive feedback: {this.countPositiveFeedbackPercentage()}</p>
-      </div>
-    );
-  }
-}
+//         <p>Neutral: {this.state.neutral}</p>
+//         <p>Bad: {this.state.bad}</p>
+//         <p>Total: {this.countTotalFeedback()}</p>
+//         <p>Positive feedback: {this.countPositiveFeedbackPercentage()}</p>
+//       </div>
+//     );
+//   }
+// }
 
-export default FeedbackCounter;
+// export default FeedbackCounter;
